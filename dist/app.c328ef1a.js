5aa02398562199e66ccddd878fd7ba0435c3c4c5
roopamgarg <roopamg777@gmail.com>
2021-11-03 17:27:12 +0530

    New Animations

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -44525,7 +44525,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44951" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42381" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
